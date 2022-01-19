5c4e28d96d4d5250fdbace2669c61d0ddb94397c
fbllobre <fbllobre@gmail.com>
2022-01-19 12:37:07 +0100

    First Commit, Eslint added

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'jsx/react-in-jsx-scope': false
+  },
+  settings: {
+    react: {
+      version: 'latest'
+    }
+  }
+}
